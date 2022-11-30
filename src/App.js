8233ec34853f715ca2aaeae0795422ac20caeec8
Vadim <savelyevresearch@protonmail.com>
2022-11-30 15:10:56 +0600

    Added the basic markup

--- src/App.js
+++ src/App.js
@@ -1,30 +1,44 @@
-import logo from "./logo.svg";
 import "./App.css";
-
-const Header = () => {
-  return (
-    <div>
-      <a>
-        Home
-      </a>
-      <a>
-        News Feed
-      </a>
-      <a>
-        Message
-      </a>
-    </div>
-  );
-};
 
 const App = () => {
   return (
-    <div className="App">
-      <Header />
-      <ul>
-        <li>First list element</li>
-        <li>Second list element</li>
-      </ul>
+    <div className="app-wrapper">
+      <header className="header">
+        <img
+          src="https://logos-download.com/wp-content/uploads/2018/02/Carolina_Panthers_logo_blue.png"
+          alt="logo"
+        />
+      </header>
+      <nav className="nav">
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+          <a>Messages</a>
+        </div>
+        <div>
+          <a>News</a>
+        </div>
+        <div>
+          <a>Music</a>
+        </div>
+        <div>
+          <a>Settings</a>
+        </div>
+      </nav>
+      <div className="content">
+        <img
+          src="https://1stwebdesigner.com/wp-content/uploads/2019/07/css-background-effects-thumb.jpg"
+          alt="some image"
+        />
+        <div>ava + description</div>
+        <div>
+          My posts
+          <div>New post</div>
+          <div>Post 1{/*  */}</div>
+          <div>Post 2</div>
+        </div>
+      </div>
     </div>
   );
 };
