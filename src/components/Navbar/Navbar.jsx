547350a83075e57f5a49346aab09fdf21eb1c754
Vadim <savelyevresearch@protonmail.com>
2022-11-30 21:52:48 +0600

    Added the NavLink components for routing without webpage refreshing

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,24 +1,27 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
-import navbarStyleClasses from './Navbar.module.css';
+import navbarStyleClasses from "./Navbar.module.css";
 
 const Navbar = () => {
+  console.log(navbarStyleClasses);
+
   return (
     <nav className={navbarStyleClasses.nav}>
       <div className={navbarStyleClasses.item}>
-        <a href="/profile">Profile</a>
+        <NavLink to="/profile" className={navData => navData.isActive ? navbarStyleClasses.active : navbarStyleClasses.item}>Profile</NavLink>
       </div>
       <div className={navbarStyleClasses.item}>
-        <a href="/dialogs">Messages</a>
+        <NavLink to="/dialogs" className={navData => navData.isActive ? navbarStyleClasses.active : navbarStyleClasses.item}>Messages</NavLink>
       </div>
       <div className={navbarStyleClasses.item}>
-        <a href="/news">News</a>
+        <NavLink to="/news" className={navData => navData.isActive ? navbarStyleClasses.active : navbarStyleClasses.item}>News</NavLink>
       </div>
       <div className={navbarStyleClasses.item}>
-        <a href="/music">Music</a>
+        <NavLink to="/music" className={navData => navData.isActive ? navbarStyleClasses.active : navbarStyleClasses.item}>Music</NavLink>
       </div>
       <div className={navbarStyleClasses.item}>
-        <a href="/settings">Settings</a>
+        <NavLink to="/settings" className={navData => navData.isActive ? navbarStyleClasses.active : navbarStyleClasses.item}>Settings</NavLink>
       </div>
     </nav>
   );
