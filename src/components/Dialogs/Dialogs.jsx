32d74e401b521452fac26a02b0c1fce6f1a78519
Vadim <savelyevresearch@protonmail.com>
2022-12-02 00:03:18 +0600

    Added the DialogItem and Message subcomponents for the Dialogs component

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,27 +1,40 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
-import dialogsStyleClasses from './Dialogs.module.css';
+import dialogsStyleClasses from "./Dialogs.module.css";
+
+const DialogItem = (props) => {
+  return (
+    <div className={`${dialogsStyleClasses.dialog}`}>
+      <NavLink
+        className={`${props.isActive ? dialogsStyleClasses.active : ""}`}
+        to={`/dialogs/${props.userId}`}
+      >
+        {props.userFirstName}
+      </NavLink>
+    </div>
+  );
+};
+
+const Message = (props) => {
+  return <div className={dialogsStyleClasses.message}>{props.messageText}</div>;
+};
 
 const Dialogs = (props) => {
   return (
     <div className={dialogsStyleClasses.dialogs}>
-        <div className={dialogsStyleClasses.dialogItems}>
-          <div className={`${dialogsStyleClasses.dialog} ${dialogsStyleClasses.active}`}>
-            <NavLink to='/dialogs/1'>Vadim</NavLink>
-          </div>
-          <div className={dialogsStyleClasses.dialog}>
-            <NavLink to='/dialogs/2'>John</NavLink>
-          </div>
-        </div>
-        <div className={dialogsStyleClasses.messages}>
-          <div className={dialogsStyleClasses.message}>
-            Hi
-          </div>
-          <div className={dialogsStyleClasses.message}>
-            Hello
-          </div>
-        </div>
+      <div className={dialogsStyleClasses.dialogItems}>
+        <DialogItem isActive={true} userId={1} userFirstName="Vadim" />
+        <DialogItem userId={2} userFirstName="John" />
+        <DialogItem userId={3} userFirstName="Stanis" />
+        <DialogItem userId={4} userFirstName="Eddard" />
+      </div>
+      <div className={dialogsStyleClasses.messages}>
+        <Message messageText="Hi" />
+        <Message messageText="Hello, how are you?" />
+        <Message messageText="Wassup..." />
+        <Message messageText="I think that you're right in this case." />
+      </div>
     </div>
   );
 };
