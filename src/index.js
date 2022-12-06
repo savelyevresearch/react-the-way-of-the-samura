9c4fcffde13c5ec5cd11ebce36929126f6110626
Vadim <savelyevresearch@protonmail.com>
2022-12-06 17:52:40 +0600

    The redux package is installed

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-import store from "./redux/state.js";
+import store from "./redux/store.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
